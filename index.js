67242de61e170a44da71d7f97264793c481e7b1b
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-20 08:17:57 -0700

    it crosses out text now

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const checkboxClass = document.getElementsByClassName("checkbox");
 
 
 // welp its what the var says
+const clearButton = document.getElementById("clearButton");
 let newTextIntoList = document.querySelector("#list-container ol");
 let newCheckboxIntoList = document.querySelector("#list-container ol li");
 
@@ -13,10 +14,26 @@ document.getElementById("accecptButtons").onclick = function(){
     createNewCheckBox.type = "checkbox";
 
     input = document.getElementById("inputText").value;
-    createNewLine.textContent = input;
+    createNewLine.appendChild(document.createTextNode(input))
+    createNewLine.className = "listText"
+    createNewCheckBox.className = "checkbox"
     // actully create it 
     newTextIntoList.appendChild(createNewLine);
     createNewLine.appendChild(createNewCheckBox);
     
    document.getElementById("inputText").value = "";
+   createNewCheckBox.onclick = function(){
+        if (createNewCheckBox.checked) {
+            createNewLine.style.textDecoration = "line-through";
+            createNewLine.style.textDecorationThickness = "2px";
+            createNewLine.style.textDecorationColor = "rgb(0,0,0)";
+        } else {
+            createNewLine.style.textDecoration = "none";
+        }
+   }
+
 } 
+clearButton.onclick = function(){
+        if (clearButton.click){console.log("clear");}
+        
+    }
