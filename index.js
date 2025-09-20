702992a202c32a4cee294ae387799d4bd619fe4b
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-19 21:51:35 -0700

    checkboxes finnaly in the right spot

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 let input;
 const checkboxClass = document.getElementsByClassName("checkbox");
-const createNewCheckBox = document.createElement("input");
+
 
 // welp its what the var says
 let newTextIntoList = document.querySelector("#list-container ol");
+let newCheckboxIntoList = document.querySelector("#list-container ol li");
 
 document.getElementById("accecptButtons").onclick = function(){
+   
     const createNewLine = document.createElement("li");
-    
+    const createNewCheckBox = document.createElement("input");
+    createNewCheckBox.type = "checkbox";
 
     input = document.getElementById("inputText").value;
     createNewLine.textContent = input;
     // actully create it 
     newTextIntoList.appendChild(createNewLine);
+    createNewLine.appendChild(createNewCheckBox);
     
-   
+   document.getElementById("inputText").value = "";
 } 
