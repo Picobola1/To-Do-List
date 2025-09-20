39f18dad72a234540f7cfaa58caec9f960585250
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-20 10:03:32 -0700

    insults play now but now randomizesd

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 let input;
 const checkboxClass = document.getElementsByClassName("checkbox");
-const AudioTest = new Audio("insultList")
+const AudioTest = new Audio('insultList/insult (14).mp3')
 
 // welp its what the var says
 const clearButton = document.getElementById("clearButton");
 let newTextIntoList = document.querySelector("#list-container ol");
 let newCheckboxIntoList = document.querySelector("#list-container ol li");
 const listContainer = document.getElementById("list-container")
+
+const InsultArray = [
+    "insultList/insult (1).mp3",
+    "insultList/insult (2).mp3",
+    "insultList/insult (3).mp3",
+    "insultList/insult (4).mp3",
+    "insultList/insult (5).mp3",
+    "insultList/insult (6).mp3",
+    "insultList/insult (7).mp3",
+    "insultList/insult (8).mp3",
+    "insultList/insult (9).mp3",
+    "insultList/insult (10).mp3",
+    "insultList/insult (11).mp3",
+    "insultList/insult (12).mp3",
+    "insultList/insult (13).mp3",
+    "insultList/insult (14).mp3",
+    "insultList/insult (15).mp3",
+    "insultList/insult (16).mp3",
+    "insultList/insult (17).mp3",
+    "insultList/insult (18).mp3",
+    "insultList/insult (19).mp3",
+    "insultList/insult (20).mp3",
+    "insultList/insult (21).mp3",
+    "insultList/insult (22).mp3",
+    "insultList/insult (23).mp3",
+    "insultList/insult (24).mp3",
+    "insultList/insult (25).mp3",
+    "insultList/insult (26).mp3",
+    "insultList/insult (27).mp3",
+    "insultList/insult (28).mp3",
+    "insultList/insult (29).mp3",
+    "insultList/insult (30).mp3",
+    "insultList/insult (31).mp3",
+    "insultList/insult (32).mp3",
+    "insultList/insult (33).mp3",
+    "insultList/insult (34).mp3",
+    "insultList/insult (35).mp3",
+    "insultList/insult (36).mp3",
+    "insultList/insult (37).mp3",
+    "insultList/insult (38).mp3",
+    "insultList/insult (39).mp3"
+]
 
 document.getElementById("accecptButtons").onclick = function(){
    
@@ -28,6 +70,9 @@ document.getElementById("accecptButtons").onclick = function(){
             createNewLine.style.textDecoration = "line-through";
             createNewLine.style.textDecorationThickness = "2px";
             createNewLine.style.textDecorationColor = "rgb(0,0,0)";
+            console.log(InsultArray[4])
+            let InsultToPlay = new Audio(InsultArray[4]);
+            InsultToPlay.play()
 
         } else {
             createNewLine.style.textDecoration = "none";
