803fcefede0e769fadaae2cb96aa34d12a7c9111
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-19 21:45:01 -0700

    text-works

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let input;
+const checkboxClass = document.getElementsByClassName("checkbox");
+const createNewCheckBox = document.createElement("input");
+
+// welp its what the var says
+let newTextIntoList = document.querySelector("#list-container ol");
 
 document.getElementById("accecptButtons").onclick = function(){
+    const createNewLine = document.createElement("li");
+    
+
     input = document.getElementById("inputText").value;
-    console.log(input);
+    createNewLine.textContent = input;
+    // actully create it 
+    newTextIntoList.appendChild(createNewLine);
+    
+   
 } 
